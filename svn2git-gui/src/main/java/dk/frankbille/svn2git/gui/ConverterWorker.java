bf286355d8beee92609048467dd1c877028a55e8
Frank Bille <github@frankbille.dk>
2013-04-01 23:03:06 +0200

    Extra log information

--- svn2git-gui/src/main/java/dk/frankbille/svn2git/gui/ConverterWorker.java
+++ svn2git-gui/src/main/java/dk/frankbille/svn2git/gui/ConverterWorker.java
@@ -2,10 +2,15 @@ package dk.frankbille.svn2git.gui;
 
 import javax.swing.SwingWorker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import dk.frankbille.svn2git.convert.Converter;
 
 public class ConverterWorker extends SwingWorker<Converter, Object> {
 
+	private static final Logger logger = LoggerFactory.getLogger(ConverterWorker.class);
+	
 	private final Converter converter;
 	
 	public ConverterWorker(Converter converter) {
@@ -14,7 +19,12 @@ public class ConverterWorker extends SwingWorker<Converter, Object> {
 
 	@Override
 	protected Converter doInBackground() throws Exception {
-		converter.convert();
+		try {
+			converter.convert();
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 		return converter;
 	}
 
