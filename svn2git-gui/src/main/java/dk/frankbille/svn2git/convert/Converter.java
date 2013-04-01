0caf202d275fa392dbb5c546927f2de5cb899be8
Frank Bille <github@frankbille.dk>
2013-04-01 23:18:35 +0200

    Fixed a bug, causing the converter to fail on Windows because of the file separator.

--- svn2git-gui/src/main/java/dk/frankbille/svn2git/convert/Converter.java
+++ svn2git-gui/src/main/java/dk/frankbille/svn2git/convert/Converter.java
@@ -33,23 +33,23 @@ import dk.frankbille.svn2git.model.MappingEntry;
 import dk.frankbille.svn2git.model.Project;
 
 public class Converter {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(Converter.class);
-	
+
 	private final Project project;
 	private final SVNClientManager svnClient;
 
 	private FileOutputStream gitFastImportFileOutputStream;
-	
+
 	private List<ConversionListener> conversionListeners = new ArrayList<>();
-	
+
 	private boolean run = true;
-	
+
 	public Converter(Project project) {
 		this.project = project;
 		svnClient = SVNClientManager.newInstance();
 	}
-	
+
 	public void convert() throws SVNException, IOException {
 		long startRevision = project.getStartRevision();
 		long endRevision;
@@ -58,17 +58,17 @@ public class Converter {
 		} else {
 			endRevision = project.getEndRevision();
 		}
-		
+
 		try {
 			for (long currentRevision = startRevision; currentRevision <= endRevision && run; currentRevision++) {
 				SVNRevision revision = SVNRevision.create(currentRevision);
-				
-				log.info("Handling Subversion revision: "+currentRevision);
-				
+
+				log.info("Handling Subversion revision: " + currentRevision);
+
 				// Find out which mapping entries is covered by the revision
 				final Set<MappingEntry> revisionEntries = new HashSet<>();
 				SVNURL svnUrl = SVNURL.parseURIEncoded(project.getSvnUrl());
-				svnClient.getLogClient().doLog(svnUrl, new String[]{"/"}, revision, revision, revision, false, true, 0, new ISVNLogEntryHandler() {
+				svnClient.getLogClient().doLog(svnUrl, new String[] { "/" }, revision, revision, revision, false, true, 0, new ISVNLogEntryHandler() {
 					@Override
 					public void handleLogEntry(SVNLogEntry logEntry) throws SVNException {
 						Map<String, SVNLogEntryPath> changedPaths = logEntry.getChangedPaths();
@@ -80,18 +80,18 @@ public class Converter {
 						}
 					}
 				});
-				
+
 				if (revisionEntries.isEmpty() && log.isDebugEnabled()) {
 					log.debug("Nothing to convert");
 				}
-				
+
 				for (MappingEntry mappingEntry : project.getMappingEntries()) {
 					if (revisionEntries.contains(mappingEntry)) {
 						checkoutOrUpdateMappingEntry(mappingEntry, revision);
 					}
 					fireMappingEntryUpdated(mappingEntry, currentRevision);
 				}
-				
+
 				processWorkspace(revision, revisionEntries);
 				fireRevisionProcessed(currentRevision);
 			}
@@ -102,28 +102,28 @@ public class Converter {
 			log.error(e.getMessage(), e);
 			throw e;
 		}
-		
+
 		IOUtils.closeQuietly(gitFastImportFileOutputStream);
 	}
-	
+
 	public void addConversionListener(ConversionListener conversionListener) {
 		conversionListeners.add(conversionListener);
 	}
-	
+
 	public void removeConversionListener(ConversionListener conversionListener) {
 		conversionListeners.remove(conversionListener);
 	}
-	
+
 	public void stop() {
 		run = false;
 	}
-	
-	private void checkoutOrUpdateMappingEntry(MappingEntry mappingEntry, SVNRevision revision) throws SVNException, IOException {		
+
+	private void checkoutOrUpdateMappingEntry(MappingEntry mappingEntry, SVNRevision revision) throws SVNException, IOException {
 		SVNUpdateClient updateClient = svnClient.getUpdateClient();
 		SVNURL svnUrl = SVNURL.parseURIEncoded(project.getSvnUrl());
-		
+
 		File mappingEntryWorkspace = new File(project.getWorkspaceFolder(), mappingEntry.getSourcePath());
-		
+
 		if (mappingEntryWorkspace.exists()) {
 			updateClient.doUpdate(mappingEntryWorkspace, revision, SVNDepth.INFINITY, true, true);
 		} else {
@@ -131,7 +131,7 @@ public class Converter {
 			updateClient.doCheckout(svnUrl.appendPath(mappingEntry.getSourcePath(), true), mappingEntryWorkspace, revision, revision, SVNDepth.INFINITY, true);
 		}
 	}
-	
+
 	private void processWorkspace(final SVNRevision revision, Set<MappingEntry> revisionEntries) throws SVNException, IOException {
 		// For each destination ref
 		Map<String, Set<MappingEntry>> destinationRefs = new HashMap<>();
@@ -143,27 +143,27 @@ public class Converter {
 			}
 			destinationRefEntries.add(mappingEntry);
 		}
-		
+
 		for (String destinationRef : destinationRefs.keySet()) {
 			final Commit commit = new Commit();
-			
+
 			SVNLogClient logClient = svnClient.getLogClient();
 			for (MappingEntry mappingEntry : destinationRefs.get(destinationRef)) {
 				File mappingEntryWorkspace = new File(project.getWorkspaceFolder(), mappingEntry.getSourcePath());
-				
+
 				try {
-					logClient.doLog(new File[]{mappingEntryWorkspace}, revision, revision, false, true, 0, new ISVNLogEntryHandler() {
+					logClient.doLog(new File[] { mappingEntryWorkspace }, revision, revision, false, true, 0, new ISVNLogEntryHandler() {
 						@Override
 						public void handleLogEntry(SVNLogEntry logEntry) throws SVNException {
 							commit.setAuthor(logEntry.getAuthor());
 							commit.setCommitDate(logEntry.getDate());
 							commit.setCommitMessage(logEntry.getMessage());
-							
+
 							Map<String, SVNLogEntryPath> changedPaths = logEntry.getChangedPaths();
 							for (Entry<String, SVNLogEntryPath> changedPathEntries : changedPaths.entrySet()) {
 								String changedPath = changedPathEntries.getKey();
 								SVNLogEntryPath svnLogEntryPath = changedPathEntries.getValue();
-								
+
 								if (svnLogEntryPath.getKind().equals(SVNNodeKind.FILE) && isPathIncluded(changedPath)) {
 									commit.addFile(new File(project.getWorkspaceFolder(), changedPath), getOperation(svnLogEntryPath.getType()));
 								}
@@ -174,41 +174,42 @@ public class Converter {
 					log.error("Suppressed exception", e);
 				}
 			}
-			
+
 			Map<File, String> files = commit.getFiles();
 			if (false == files.isEmpty()) {
-				appendToFastImportFile("commit "+destinationRef);
+				appendToFastImportFile("commit " + destinationRef);
 				appendNewlineToFastImportFile();
-				appendToFastImportFile("mark :"+revision.getNumber());
+				appendToFastImportFile("mark :" + revision.getNumber());
 				appendNewlineToFastImportFile();
-				appendToFastImportFile("committer "+getGitAuthor(commit.getAuthor())+" "+commit.getCommitDate().getTime()/1000+" +0100");
+				appendToFastImportFile("committer " + getGitAuthor(commit.getAuthor()) + " " + commit.getCommitDate().getTime() / 1000 + " +0100");
 				appendNewlineToFastImportFile();
-				appendToFastImportFile("data "+commit.getCommitMessage().getBytes("UTF-8").length);
+				appendToFastImportFile("data " + commit.getCommitMessage().getBytes("UTF-8").length);
 				appendNewlineToFastImportFile();
 				appendToFastImportFile(commit.getCommitMessage());
 				appendNewlineToFastImportFile();
 				for (File file : files.keySet()) {
 					String operation = files.get(file);
 					String filePath = file.getAbsolutePath().replace(project.getWorkspaceFolder(), "");
+					filePath = filePath.replace(System.getProperty("file.separator"), "/");
 					MappingEntry mappingEntry = getEntryForPath(filePath);
 					filePath = filePath.replace(mappingEntry.getSourcePath(), mappingEntry.getDestinationPath());
-					
+
 					if ("M".equals(operation)) {
-						appendToFastImportFile("M 644 inline "+filePath);
+						appendToFastImportFile("M 644 inline " + filePath);
 						appendNewlineToFastImportFile();
-						appendToFastImportFile("data "+file.length());
+						appendToFastImportFile("data " + file.length());
 						appendNewlineToFastImportFile();
 						appendToFastImportFile(file);
 						appendNewlineToFastImportFile();
 					} else if ("D".equals(operation)) {
-						appendToFastImportFile("D "+filePath);
+						appendToFastImportFile("D " + filePath);
 						appendNewlineToFastImportFile();
 					}
 				}
 			}
 		}
 	}
-	
+
 	private String getOperation(char svnType) {
 		switch (svnType) {
 		case 'M':
@@ -218,68 +219,68 @@ public class Converter {
 		case 'D':
 			return "D";
 		}
-		
-		throw new IllegalArgumentException("Don't know how to handle "+svnType);
+
+		throw new IllegalArgumentException("Don't know how to handle " + svnType);
 	}
-	
+
 	private MappingEntry getEntryForPath(String path) {
 		for (MappingEntry mappingEntry : project.getMappingEntries()) {
 			if (path.startsWith(mappingEntry.getSourcePath())) {
 				return mappingEntry;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	private boolean isPathIncluded(String path) {
 		for (MappingEntry mappingEntry : project.getMappingEntries()) {
 			if (path.contains(mappingEntry.getSourcePath())) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private void appendNewlineToFastImportFile() throws IOException {
 		appendToFastImportFile("\n");
 	}
-	
+
 	private void appendToFastImportFile(CharSequence text) throws IOException {
 		IOUtils.write(text, getGitFastImportFileOutputStream(), "UTF-8");
 		fireGitFastImportFileChanged(text.toString().getBytes("UTF-8").length);
 	}
-	
+
 	private void appendToFastImportFile(File file) throws IOException {
 		int bytesCopied = IOUtils.copy(new FileInputStream(file), getGitFastImportFileOutputStream());
 		fireGitFastImportFileChanged(bytesCopied);
 	}
-	
+
 	private OutputStream getGitFastImportFileOutputStream() throws FileNotFoundException {
 		if (gitFastImportFileOutputStream == null) {
 			gitFastImportFileOutputStream = new FileOutputStream(project.getGitFastImportFile(), true);
 		}
-		
+
 		return gitFastImportFileOutputStream;
 	}
-	
+
 	private String getGitAuthor(String svnUsername) throws FileNotFoundException, IOException {
 		return project.getGitAuthor(svnUsername);
 	}
-	
+
 	private void fireRevisionProcessed(long revisionNumber) {
 		for (ConversionListener conversionListener : conversionListeners) {
 			conversionListener.revisionProcessed(revisionNumber);
 		}
 	}
-	
+
 	private void fireMappingEntryUpdated(MappingEntry mappingEntry, long revisionNumber) {
 		for (ConversionListener conversionListener : conversionListeners) {
 			conversionListener.mappingEntryUpdated(mappingEntry, revisionNumber);
 		}
 	}
-	
+
 	private void fireGitFastImportFileChanged(int numberOfBytesAppended) {
 		for (ConversionListener conversionListener : conversionListeners) {
 			conversionListener.gitFastImportFileChanged(new File(project.getGitFastImportFile()), numberOfBytesAppended);
